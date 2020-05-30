4b56c51087b3188f77f510b5ea0150268aae11cb
Eric Evenchick <eric@evenchick.com>
2020-05-30 18:44:28 -0400

    write to file instead of stdout

--- src/bin/candump.rs
+++ src/bin/candump.rs
@@ -1,13 +1,17 @@
 use cantact::{Frame, Interface};
 use std::thread;
 use std::time::Duration;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn print_frame(f: Frame) {
-    print!("  ch:{}  {:03X}   [{}]  ", f.channel, f.can_id, f.can_dlc);
+fn print_frame(file: &mut File, f: Frame) {
+    let mut s = format!("  ch:{}  {:03X}   [{}]  ", f.channel, f.can_id, f.can_dlc);
     for b in f.data.iter().take(f.can_dlc as usize) {
-        print!("{:02X} ", b);
+        s = format!("{}{:02X} ", s, b);
     }
-    println!("");
+    s = format!("{}\n", s);
+    file.write_all(s.as_bytes()).unwrap();
+    
 }
 
 fn main() {
@@ -16,10 +20,12 @@ fn main() {
     // configure the CAN channel
     i.set_bitrate(0, 500000).expect("error setting bitrate");
 
+    let mut file = File::create("log.txt").expect("error opening output file");
+
     // start the device
     // provides a closure to be called when a frame is received
     i.start(move|f: Frame| {
-        print_frame(f);
+        print_frame(&mut file, f);
     })
     .expect("failed to start device");
 
