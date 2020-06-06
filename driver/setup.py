26c961d993a56ace731334b99e659c84969b75f8
Eric Evenchick <eric@evenchick.com>
2020-06-06 11:36:27 -0400

    release version 0.0.4

--- driver/setup.py
+++ driver/setup.py
@@ -23,7 +23,7 @@ with open("../README.md", "r") as fh:
 
 setup(
     name="cantact",
-    version="0.0.3",
+    version="0.0.4",
     author="Eric Evenchick",
     author_email="eric@evenchick.com",
     description="Support for the CANtact Controller Area Network Devices",
