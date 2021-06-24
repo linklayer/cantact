a79a416286d9cc7d7d4090792963bba6744f3ea0
Eric Evenchick <eric@evenchick.com>
2021-06-24 10:08:42 -0700

    use cfg values for send command

--- src/send.rs
+++ src/send.rs
@@ -1,20 +1,39 @@
 use crate::Error;
 use cantact::{Frame, Interface};
 use clap::ArgMatches;
+use log::info;
 use std::thread;
 use std::time::Duration;
 
+use crate::config::Config;
 use crate::helpers;
 
-pub fn cmd(_matches: &ArgMatches) -> Result<(), Error> {
+pub fn cmd(matches: &ArgMatches) -> Result<(), Error> {
     let flag = helpers::initialize_ctrlc();
 
+    let mut config = Config::read();
+
+    let ch = helpers::parse_channel(&matches)?;
+    match ch {
+        None => { /* no channel specified, follow config */ }
+        Some(ch) => {
+            // channel specified, disable all others
+            for n in 0..config.channels.len() {
+                if n != ch {
+                    config.channels[n].enabled = false;
+                }
+            }
+        }
+    }
+    info!("config: {:?}", config);
+
     // initialize the interface
-    let mut i = Interface::new().expect("error opening device");
-    // configure the CAN channel
-    i.set_bitrate(0, 500_000).expect("error setting bitrate");
+    let mut i = Interface::new()?;
+    config.apply_to_interface(&mut i)?;
+
     // start the device
-    i.start(|_: Frame| {}).expect("failed to start device");
+    info!("starting dump");
+    i.start(move |_: Frame| {}).expect("failed to start device");
 
     let mut count = 0;
     let mut f = Frame::default();
